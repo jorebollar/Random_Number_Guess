53d92e22c871741b0774c6d2d381509689e6c2e2
jorebollar <jorebollar@outlook.com>
2023-02-25 11:41:25 -0600

    Made changes to loop statements and corrected indentation.

--- Random_Number_Guess.py
+++ Random_Number_Guess.py
@@ -3,23 +3,18 @@ import random
 
 #Random number genrator between 1 and 10
 number = random.randint(1, 10)
-
-#Ask user input between 1 and 10
-guess = input("Guess a number between 1 and 10: ")
-
-#User input string converted to an integer
-guess = int(guess)
-
-#Checking user guess
-if guess == number:
-    print("You guessed correctly!")
-elif guess < number:
-    print("You guessed low")
-elif guess > number:
-    print("You guessed high")
-
 guesses = 0
 
-while guess != number:
-
-    print("Congrats, you guessed the number in" , guesses, "guesses!")
+while True:
+   guess = input("Guess a number between 1 and 10: ")
+   guess = int(guess)
+   guesses += 1
+    
+#Checking user guess
+   if guess == number:
+        print("You guessed correctly!")
+        break
+   elif guess < number:
+        print("You guessed low")
+   else:
+        print("You guessed high")
